c1e1012072f64df378a59f34bf8d38a58acedf2b
Steve <stevejamesdaniels@outlook.com>
2019-12-02 21:29:42 +0000

    Code formatting

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 // import Game from '../Game'
-import { observer } from 'mobx-react'
-import { gameState, Store } from '../../Store'
+import { observer } from "mobx-react";
+import { gameState, Store } from "../../Store";
 
 const AppProps = {
   gameState: Store
 };
 
 const App: React.FunctionComponent<{
-  gameState: Store
+  gameState: Store;
 }> = observer(props => {
   return (
-    <div className='App-container'>
+    <div className="App-container">
       <div className='container'>
-      <h1>Hello world!</h1>
-      <p>{gameState.currentPlayer}</p>
-      <button onClick={gameState.nextPlayer} type='button'>Change Player</button>
-      </div>
+        <h1>Hello world!</h1>
+        <p>{gameState.currentPlayer}</p>
+        <button onClick={gameState.nextPlayer} type="button">
+          Change Player
+        </button>
+        <p>Hello</p>
+      </Game>
     </div>
   );
 });
 
-export default App
+export default App;
