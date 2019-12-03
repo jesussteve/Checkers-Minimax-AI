b5f4684a8698407c65725a8bd531d0eba678668a
Steve <stevejamesdaniels@outlook.com>
2019-12-03 02:58:09 +0000

    React component for checker playing pieces

---
+++ src/Components/Checker/Checker.tsx
@@ -0,0 +1,42 @@
+/**
+ * @file Checker React component
+ */
+import React, { FunctionComponent } from 'react'
+import './Checker.css'
+
+/**
+ * Props
+ */
+type CheckerProps = {
+    onClick: (squareId: number, player: string, isKing: boolean) => void
+    currentSquare: number
+    player: string
+    isKing?: boolean
+    squareId: number
+}
+/**
+ * @constructor React Function Component Declaration
+ * @param {object} onClick - onClick Event when checker is selected
+ * @param {number} currentSquare - The currently selected square on the board
+ * @param {string} player - The current player ('black'|'white')
+ * @param {boolean} isKing - Flag for checker king status
+ * @param {number} squareId - The current square for the checker
+ */
+export const Checker: FunctionComponent<CheckerProps> = ({
+    // Prop Defaults
+    onClick,
+    currentSquare,
+    player,
+    isKing = false,
+    squareId
+}) =>
+    // Render Component
+    <div onClick={() => onClick(squareId, player, isKing)}
+        className={` Checker 
+                    ${player === 'white' ? "Checker-white" : "Checker-black"}
+                    ${squareId === currentSquare ? 'Square-selected' : ''}
+                    ${isKing ? 'Square-king' : ''} 
+                    `}>
+    </div>
+
+export default Checker
